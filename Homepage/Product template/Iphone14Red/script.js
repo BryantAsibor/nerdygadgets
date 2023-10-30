fb2a888e31f07ac7d241f4846db00011c6acd7fb
Bryant Asibor <2136149@talnet.nl>
2023-10-30 19:51:41 +0100

    Begin voorpagina (Met css stylesheet)

--- Homepage/Product template/Iphone14Red/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-      $('.active').removeClass('active');
-      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-      $(this).addClass('active');
-  });
-
-});
