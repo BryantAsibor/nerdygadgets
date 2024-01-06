8c23633a9dd5d640f4a83f02c692a527828f333e
twan010601 <t.nobbenhuis@gmail.com>
2024-01-06 22:07:51 +0100

    search bar statisch

--- Homepage/Shop/product.php
+++ Homepage/Shop/product.php
@@ -87,6 +87,9 @@ mysqli_close($connection);
                     <li><a href="../OverOnsBryce/overonstest.php">Over ons</a></li>
                     <li><a href="../Contact/contact.php">Contact</a></li>
                 </ul>
+                <div class="seachbar">
+                <input type="sumbit" placeholder="Search..">
+                </div>
                 <div class="cart"><i class="fa-solid fa-cart-shopping"></i><p id="count">0</p></div>
             </div>
         </nav>
